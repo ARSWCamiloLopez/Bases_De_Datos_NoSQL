336f3374e4bd9d0744fc996a66b013bc0b03c749
Camilo Lopez <camilino123@gmail.com>
2018-11-08 18:03:25 -0500

    Union interfaz grafica

---
+++ src/main/java/edu/eci/arsw/parcial/model/AccionRepository.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.parcial.model;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ *
+ * @author camilo
+ */
+public interface AccionRepository extends MongoRepository<Accion, String>{
+    
+}
